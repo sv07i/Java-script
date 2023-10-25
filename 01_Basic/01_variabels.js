5c6a3cf40356f451fa304b20b8528d4bc70e58fc
sv07i <71116242+sv07i@users.noreply.github.com>
2023-10-25 05:13:54 +0000

    variables

---
+++ 01_Basic/01_variabels.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountName = "Yash"
+var accountEmail = "yash@gmail.com"
+accountCity = "Himatnagar"
+
+//accountId = 9999
+//console.log("accountId") // not allowed becaz its define as const
+
+//mostly people use let not use var
+//without define let and var we can define anything and system gives space for that.
+
+accountName = "Neel"
+accountEmail = "neel@gmail.com"
+accountCity = "Gandhinagar"
+
+console.table([accountName,accountEmail,accountCity])
