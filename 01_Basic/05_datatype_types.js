f0301a9918103e8819911d0319c4619d363f9d52
sv07i <71116242+sv07i@users.noreply.github.com>
2023-10-27 04:16:36 +0000

    chnage

---
+++ 01_Basic/05_datatype_types.js
@@ -0,0 +1,32 @@
+//primitive 
+// number , string, bigint, boolean,null,undefined,symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+
+//Non primitive
+
+//Array, object, functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myobj = {
+    name : "yash"
+    age : 23
+}
+
+const myfunction = function(){
+    console.log("hello yash")
+}
+
